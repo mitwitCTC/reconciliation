b625ddf80127be7c283f05f574ad2a729079be31
Eileen-Qian <yiqianc.14@gmail.com>
2024-05-02 16:35:56 +0800

    Updates

--- assets/MonthlyrentalView-p9okE0-R.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,c as s,a as t,t as n,o as a}from"./index-PBjipKIi.js";const c={data(){return{text:"月租"}}},r={class:"container"},_=t("p",null,"頁面建置中...",-1);function l(i,p,d,u,e,f){return a(),s("div",r,[t("p",null,n(e.text)+"對帳",1),_])}const h=o(c,[["render",l]]);export{h as default};
