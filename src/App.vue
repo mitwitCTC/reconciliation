15753cd1996510f30cdd02be5e61a3db5267ce70
Eileen-Qian <yiqianc.14@gmail.com>
2024-05-20 16:01:21 +0800

    feat: update layout
    Not compatible with mobile version

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,24 @@ export const API = 'http://122.116.23.30:3335'
 </script>
 
 <template>
-  <RouterView />
+  <div class="md">
+    <RouterView />
+  </div>
+  <div class="sm text-center">
+    <p>不支援手機，請使用平板或電腦</p>
+  </div>
 </template>
 
 <style scoped>
-
+.sm {
+  display: none;
+}
+@media screen and (max-width:767px) { 
+  .md {
+    display: none;
+  }
+  .sm {
+    display: block;
+  }
+}
 </style>
