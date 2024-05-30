22ac13396e7c4f274133dc49cd1cf0c1ea958a9c
Eileen-Qian <yiqianc.14@gmail.com>
2024-05-30 13:45:14 +0800

    feat: add group name and logout button

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -4,6 +4,25 @@ import NavTabs from '@/components/NavTabs.vue'
 export default {
   components: {
     NavTabs
+  },
+  data() {
+    return {
+      groupId: ''
+    }
+  },
+  methods: {
+    logout() {
+      sessionStorage.removeItem('isAuthenticated')
+      alert('登出成功')
+      this.$router.push('/login')
+    },
+    // 取集團代號
+    getgroup() {
+      this.groupId = atob(this.$route.params.group)
+    }
+  },
+  mounted() {
+    this.getgroup()
   }
 }
 </script>
@@ -15,6 +34,10 @@ export default {
         <NavTabs />
       </div>
       <div class="col-9">
+        <div class="d-flex align-items-center custom-container">
+          <h2 class="mb-0">力揚集團</h2>
+          <el-button class="header-button" @click="logout()">登出</el-button>
+        </div>
         <RouterView />
       </div>
     </div>
