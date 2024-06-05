7ff772a1d32ca1b1c77be2e34472a115216a1a35
Eileen-Qian <yiqianc.14@gmail.com>
2024-06-05 17:09:55 +0800

    feat: modify header and update logout function
    調整 header 樣式、登出時移除 sessionStorage 裡的 activeMenu 和 account

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -13,6 +13,8 @@ export default {
   methods: {
     logout() {
       sessionStorage.removeItem('isAuthenticated')
+      sessionStorage.removeItem('activeMenu')
+      sessionStorage.removeItem('account')
       alert('登出成功')
       this.$router.push('/login')
     },
@@ -28,22 +30,61 @@ export default {
 </script>
 
 <template>
-  <div class="container mt-3">
-    <div class="row">
-      <div class="col-3">
-        <NavTabs />
-      </div>
-      <div class="col-9">
-        <div class="d-flex align-items-center custom-container">
-          <h2 class="mb-0">力揚集團</h2>
-          <el-button class="header-button" @click="logout()">登出</el-button>
+  <div class="common-layout">
+    <el-container>
+      <el-header class="d-flex justify-content-between align-items-center">
+        <h2>力揚帳務</h2>
+        <div class="d-flex align-items-center logout" @click="logout()">
+          <el-icon><SwitchButton /></el-icon>
+          登出
         </div>
-        <RouterView />
-      </div>
-    </div>
+      </el-header>
+      <el-container>
+        <el-aside><NavTabs /></el-aside>
+        <el-main><RouterView /></el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <style>
+.el-header {
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+  padding: 10px 0;
+  display: flex;
+}
 
+.el-header h2 {
+  cursor: default;
+}
+
+.logout {
+  cursor: pointer;
+  font-weight: bold;
+  margin-right: 95px;
+  &:hover {
+    color: #f86c6c;
+  }
+}
+
+.el-aside {
+  margin-top: 5px;
+  height: 90vh;
+  overflow-y: auto;
+  max-width: 10%;
+}
+
+.el-main {
+  max-width: 90%;
+}
+
+@media (max-width: 1024px) {
+  .el-aside {
+    max-width: 20%;
+  }
+
+  .el-main {
+    max-width: 80%;
+  }
+}
 </style>
