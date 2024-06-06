331c36a3e19d7837ad8d70f173bac01c14fd8611
Eileen-Qian <yiqianc.14@gmail.com>
2024-06-06 14:40:49 +0800

    feat: modify common-layout(add aside-toggle btn)
    加入側邊欄收合按鈕、Register All Icons

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -3,11 +3,12 @@ import NavTabs from '@/components/NavTabs.vue'
 
 export default {
   components: {
-    NavTabs
+    NavTabs,
   },
   data() {
     return {
-      groupId: ''
+      groupId: '',
+      isAsideCollapsed: false
     }
   },
   methods: {
@@ -21,6 +22,9 @@ export default {
     // 取集團代號
     getgroup() {
       this.groupId = atob(this.$route.params.group)
+    },
+    toggleAside() {
+      this.isAsideCollapsed = !this.isAsideCollapsed
     }
   },
   mounted() {
@@ -33,15 +37,26 @@ export default {
   <div class="common-layout">
     <el-container>
       <el-header class="d-flex justify-content-between align-items-center">
-        <h2>力揚帳務</h2>
+        <div class="d-flex align-items-center">
+          <div class="aside-toggle" @click="toggleAside" :class="{ 'collapsed': isAsideCollapsed }">
+            <el-icon size="20px" color="#004080" v-if="isAsideCollapsed"><Expand /></el-icon>
+            <el-icon size="20px" color="#004080" v-else><Fold /></el-icon>
+          </div>
+          <h2>力揚帳務</h2>
+        </div>
         <div class="d-flex align-items-center logout" @click="logout()">
           <el-icon><SwitchButton /></el-icon>
           登出
         </div>
       </el-header>
       <el-container>
-        <el-aside><NavTabs /></el-aside>
-        <el-main><RouterView /></el-main>
+        <el-aside :width="isAsideCollapsed ? '0' : '200px'" class="aside-container">
+          <NavTabs />
+        </el-aside>
+        <el-main>
+          <RouterView />
+          <div v-if="!isAsideCollapsed" class="overlay" @click="toggleAside"></div>
+        </el-main>
       </el-container>
     </el-container>
   </div>
@@ -52,6 +67,7 @@ export default {
   box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
   padding: 10px 0;
   display: flex;
+  justify-content: space-between;
 }
 
 .el-header h2 {
@@ -62,29 +78,48 @@ export default {
   cursor: pointer;
   font-weight: bold;
   margin-right: 95px;
+  display: flex;
+  align-items: center;
   &:hover {
     color: #f86c6c;
   }
 }
 
 .el-aside {
-  margin-top: 5px;
   height: 90vh;
   overflow-y: auto;
-  max-width: 10%;
+  transition: width 0.5s;
+  /* 移除 scrollbar 樣式 */
+  -ms-overflow-style: none;  /* IE and Edge */
+  scrollbar-width: none;  /* Firefox */
+}
+
+.el-aside::-webkit-scrollbar {
+  /* 移除 scrollbar 樣式 */
+  display: none; /* Chrome, Safari, Opera */
+}
+
+.aside-container {
+  position: relative;
+}
+
+.aside-toggle {
+  cursor: pointer;
+  padding: 0 5px;
+  margin-right: 10px;
 }
 
 .el-main {
-  max-width: 90%;
+  position: relative;
 }
 
-@media (max-width: 1024px) {
-  .el-aside {
-    max-width: 20%;
-  }
-
-  .el-main {
-    max-width: 80%;
-  }
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.3);
+  z-index: 2;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -16,6 +16,9 @@ import VueAxios from 'vue-axios';
 import axios from 'axios';
 
 const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 
 app.use(createPinia())
 app.use(router)
